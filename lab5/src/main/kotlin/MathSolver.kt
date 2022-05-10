2de4ad29d79ed321aba472118ae721327aa72fac
kamilchulakov <mr1english4@gmail.com>
2022-05-10 11:18:48 +0300

    Add working Gauss method

--- lab5/src/main/kotlin/MathSolver.kt
+++ lab5/src/main/kotlin/MathSolver.kt
@@ -27,8 +27,68 @@ object MathSolver {
         st.interpolationValues.add(InterpolationValue("Многочлен Лагранжа", resPoints.sum().prettyRound()))
     }
 
-    fun gaussInterpolation(st: ProgramState) {
+    fun tMult(t: Double, i: Int): Double {
+        var temp = t
+        for (j in 1 until i) {
+            temp *= if (j % 2 == 1) {
+                (t - j)
+            } else
+                (t + j)
+        }
+        return temp
+    }
 
+    fun gaussInterpolation(st: ProgramState) {
+        val otherY = mutableListOf<MutableList<Double>>()
+//        И давно все узлы развязаны
+//        А на дожде — все дороги радугой!
+//        Быть беде. Нынче нам до смеха ли?
+//        Но если есть колокольчик под дугой
+//                Так, значит, все. Заряжай — поехали!
+//        Загремим, засвистим, защелкаем!
+//        Проберет до костей, до кончиков
+//        Эй! братва! Чуете печенками грозный смех
+//        Русских колокольчиков?
+//        Век жуем. Матюги с молитвами
+//                Век живем — хоть шары нам выколи
+//        Спим да пьём. Сутками и литрами
+//        Не поем. Петь уже отвыкли
+//                Долго ждём. Все ходили грязные
+//                Оттого сделались похожие
+//        А под дождём оказались разные
+//        Большинство — честные, хорошие
+//        И, пусть разбит батюшка Царь-колокол
+//        Мы пришли. Мы пришли с гитарами
+//        Ведь биг-бит, блюз и рок-н-ролл
+//        Околдовали нас первыми ударами
+//                И в груди — искры электричества
+//        Шапки в снег — и рваните звонче
+//        Рок-н-ролл! Славное язычество
+//        Я люблю время колокольчиков
+        for (i in 0 until st.n) {
+            otherY.add(mutableListOf())
+            for (j in 0 until st.n - i) {
+                otherY[i].add(0.0)
+            }
+        }
+        for (i in 0 until st.n) {
+            otherY[i][0]=st.y[i]
+        }
+
+        for (i in 1 until st.n) {
+            for (j in 0 until st.n - i) {
+                otherY[j][i]=
+                    (otherY[j + 1][i - 1] - otherY[j][i - 1]).prettyRound()
+            }
+        }
+
+        var sum = otherY[(st.n/2)][0]
+        val t = (st.interpolationParam - st.x[(st.n/2)]) / (st.x[1] - st.x[0])
+
+        for (i in 1 until st.n) {
+            sum += (tMult(t, i) * otherY[((st.n - i) / 2)][i]) / i
+        }
+        st.interpolationValues.add(InterpolationValue("Многочлен Гаусса", sum.prettyRound()))
     }
 }
 
